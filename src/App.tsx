faace73a69ed290b16eb6eb3811b1d2977f05cf4
Vibesharing-App <chris_avore@me.com>
2026-04-03 14:21:17 -0400

    Polish nav, meetings detail page, and global layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { AppLayout } from "@diligentcorp/atlas-react-bundle";
 import { Outlet, Route, Routes } from "react-router";
 import "./styles.css";
-
-import Navigation from "./Navigation.js";
-import HomePage from "./pages/HomePage.js";
-import MeetingsPage from "./pages/MeetingsPage.js";
-import PlaceholderPage from "./pages/PlaceholderPage.js";
+import IndexPage from "./pages/IndexPage";
 
 export default function App() {
   return (
@@ -13,20 +9,12 @@ export default function App() {
       <Route
         path="/"
         element={
-          <AppLayout navigation={<Navigation />}>
+          <AppLayout navigation={[]}>
             <Outlet />
           </AppLayout>
         }
       >
-        <Route index element={<HomePage />} />
-        <Route path="meetings" element={<MeetingsPage />} />
-        <Route path="agenda" element={<PlaceholderPage title="Agenda items" />} />
-        <Route path="policies" element={<PlaceholderPage title="Policies" />} />
-        <Route path="library/files" element={<PlaceholderPage title="Files" />} />
-        <Route path="library/goals" element={<PlaceholderPage title="Goals" />} />
-        <Route path="library/events" element={<PlaceholderPage title="Events" />} />
-        <Route path="library/members" element={<PlaceholderPage title="Board members" />} />
-        <Route path="settings" element={<PlaceholderPage title="Settings" />} />
+        <Route index element={<IndexPage />} />
       </Route>
     </Routes>
   );
